044a40739dd3c51d4881f08bc7ae5f1fc7340498
Jeffrey Vandenborne <vandenbornejeffrey@gmail.com>
2015-03-07 13:39:23 +0100

    added plugin feature

--- index.js
+++ index.js
@@ -1,12 +1,17 @@
-var Elixir = function(recipe) {
+"use strict";
+
+var loadPlugins = require('gulp-load-plugins');
+
+var Elixir = function (recipe) {
     require('require-dir')('./ingredients');
     recipe(Elixir.config);
 };
 
 Elixir.config = require('./Config');
 Elixir.config.setDefaultsFrom('elixir.json');
+Elixir.$ = loadPlugins();
 
-Elixir.extend = function(name, callback) {
+Elixir.extend = function (name, callback) {
     Elixir.config[name] = callback;
 };
 
